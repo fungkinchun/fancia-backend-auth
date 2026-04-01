cf5f3149c79ae65cf33ddf7e8028f8e59f4d2a11
fungkinchun <fungkinchun@gmail.com>
2026-04-01 13:47:51 +0800

    first commit

--- src/test/kotlin/com/fancia/backend/auth/AuthApplicationTests.kt
+++ src/test/kotlin/com/fancia/backend/auth/AuthApplicationTests.kt
@@ -2,32 +2,13 @@ package com.fancia.backend.auth
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.DynamicPropertyRegistry
-import org.springframework.test.context.DynamicPropertySource
-import org.testcontainers.containers.PostgreSQLContainer
-import org.testcontainers.junit.jupiter.Container
+import org.springframework.context.annotation.Import
 import org.testcontainers.junit.jupiter.Testcontainers
 
 @Testcontainers
 @SpringBootTest
+@Import(TestConfig::class)
 class AuthApplicationTests {
-    companion object {
-        @Container
-        @JvmField
-        val postgres: PostgreSQLContainer<*> = PostgreSQLContainer("postgres:16")
-            .withDatabaseName("testdb")
-            .withUsername("test")
-            .withPassword("test")
-
-        @JvmStatic
-        @DynamicPropertySource
-        fun registerPgProperties(registry: DynamicPropertyRegistry) {
-            registry.add("spring.datasource.url") { postgres.jdbcUrl }
-            registry.add("spring.datasource.username") { postgres.username }
-            registry.add("spring.datasource.password") { postgres.password }
-        }
-    }
-
     @Test
     fun contextLoads() {
     }
